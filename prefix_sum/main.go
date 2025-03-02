15d44b8877841db4c2a93e902d25bbc80685be00
nahidfarazi <nahidfarazi19@gmail.com>
2025-03-02 12:28:02 +0600

    prefix-sum

--- prefix_sum/main.go
+++ prefix_sum/main.go
@@ -10,14 +10,13 @@ func main() {
 	for i := 0; i < n; i++ {
 		fmt.Scanf("%d", &a[i])
 	}
-	fmt.Println(a)
-	fmt.Println(q)
+
 	var pre = make([]int, n)
 	pre[0] = a[0]
 	for i := 1; i < n; i++ {
 		pre[i] = a[i] + pre[i-1]
 	}
-	fmt.Println(pre)
+
 	for i := 0; i < q; i++ {
 		var l, r int
 		fmt.Scanf("%d %d", &l, &r)
